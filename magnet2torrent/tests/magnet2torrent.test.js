c44c0ce7c5aa34085d5eb5ccaa93cfe70f63691e
Alessandro Aglietti <alessandro.aglietti@brumbrum.it>
2021-09-01 00:11:13 +0200

    create buffer between gettorrentinfo timeout and jest timeout

--- magnet2torrent/tests/magnet2torrent.test.js
+++ magnet2torrent/tests/magnet2torrent.test.js
@@ -8,7 +8,7 @@ test('Voyagers (2021) BluRay 1080p.H264 Ita Eng AC3 5.1 Sub Ita Eng', async () =
 
 test('getTorrentInfo', async () => {
     const magnet = "magnet:?xt=urn:btih:7FA6EC9C323CA54A33DF16C8F41272A41A8ACB32&dn=Voyagers+%282021%29+BluRay+1080p.H264+Ita+Eng+AC3+5.1+Sub+Ita+Eng+-+realDMDJ&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.com%3A2710%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.iamhansen.xyz%3A2000%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Fipv4.tracker.harry.lu%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.zer0day.to%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Fcoppersurfer.tk%3A6969%2Fannounce";
-    const torrentInfos = await getTorrentInfo(magnet);
+    const torrentInfos = await getTorrentInfo(magnet, 50000);
     expect(torrentInfos).toHaveProperty('invokedAt');
     expect(torrentInfos).toHaveProperty('errors');
     expect(torrentInfos.errors.length).toBe(0);
